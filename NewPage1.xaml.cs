1baee590b64c7d30dc10ab9dff63b6906dbe7c3a
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-06-11 16:46:41 -0500

    Paginacion

--- NewPage1.xaml.cs
+++ NewPage1.xaml.cs
@@ -1,9 +1,47 @@
 namespace Navegacion0;
+using Microsoft.Maui.Controls;
 
 public partial class NewPage1 : ContentPage
-{
-	public NewPage1()
-	{
-		InitializeComponent();
-	}
-}
+    {
+        public NewPage1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void ChangePage(object sender, EventArgs e)
+        {
+            if (sender is Button button && int.TryParse(button.Text, out int pageNumber))
+            {
+                // Hide all labels
+                page1Label.IsVisible = false;
+                page2Label.IsVisible = false;
+                page3Label.IsVisible = false;
+                page4Label.IsVisible = false;
+                page5Label.IsVisible = false;
+
+
+
+                // Show the corresponding label for the selected page
+                switch (pageNumber)
+                {
+                    case 1:
+                        page1Label.IsVisible = true;
+                        break;
+                    case 2:
+                        page2Label.IsVisible = true;
+                        break;
+                    case 3:
+                        page3Label.IsVisible = true;
+                        break;
+                    case 4:
+                        page4Label.IsVisible = true;
+                        break;
+                    case 5:
+                        page5Label.IsVisible = true;
+                        break;
+                }
+            }
+        }
+    }
